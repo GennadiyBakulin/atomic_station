92eba1684d2f3a51b077e63c28e9209ff88b12ee
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-11-03 17:21:18 +0700

    Выполнил Этап 1 пункт 10

--- src/main/java/com/javaacademy/nuclearstation/Runner.java
+++ src/main/java/com/javaacademy/nuclearstation/Runner.java
@@ -1,12 +1,17 @@
 package com.javaacademy.nuclearstation;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Runner {
 
   public static void main(String[] args) {
+    ConfigurableApplicationContext context = SpringApplication.run(Runner.class, args);
 
+    NuclearStation nuclearStation = context.getBean(NuclearStation.class);
+    nuclearStation.start(3);
+    context.close();
   }
-
 }
