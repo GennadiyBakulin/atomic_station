70ef319318faba321d8bb16afe6e0169aa1efa00
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-11-03 18:30:15 +0700

    Выполнил этап 2 пункт 5.

--- src/main/java/com/javaacademy/nuclearstation/reactordepartment/ReactorDepartment.java
+++ src/main/java/com/javaacademy/nuclearstation/reactordepartment/ReactorDepartment.java
@@ -35,12 +35,23 @@ public class ReactorDepartment {
   }
 
   /**
+   * Метод - остановить реактор.
+   *
+   * @throws ReactorWorkException - выбрасывается если проверка показала, что реактор уже выключен.
+   */
+  public void stop() throws ReactorWorkException {
+    checkIsWorkingReactor();
+    isWorking = false;
+  }
+
+  /**
    * Метод - проверка на то, что реактор отключен.
    *
    * @throws ReactorWorkException - выбрасывается если проверка показала, что реактор уже работал.
    */
   private void checkIsNotWorkingReactor() throws ReactorWorkException {
     if (isWorking) {
+      securityDepartment.addAccident();
       throw new ReactorWorkException("Реактор уже работает");
     }
   }
@@ -53,18 +64,9 @@ public class ReactorDepartment {
   private void checkEveryHundredRuns() throws NuclearFuelIsEmptyException {
     if (counterLaunch == 100) {
       counterLaunch = 0;
+      securityDepartment.addAccident();
       throw new NuclearFuelIsEmptyException();
     }
-  }
-
-  /**
-   * Метод - остановить реактор.
-   *
-   * @throws ReactorWorkException - выбрасывается если проверка показала, что реактор уже выключен.
-   */
-  public void stop() throws ReactorWorkException {
-    checkIsWorkingReactor();
-    isWorking = false;
   }
 
   /**
@@ -74,6 +76,7 @@ public class ReactorDepartment {
    */
   private void checkIsWorkingReactor() throws ReactorWorkException {
     if (!isWorking) {
+      securityDepartment.addAccident();
       throw new ReactorWorkException("Реактор уже выключен");
     }
   }
