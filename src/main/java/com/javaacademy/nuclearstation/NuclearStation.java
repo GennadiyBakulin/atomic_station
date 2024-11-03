b8209746276c0defd3e71470ecae18901a5f87bf
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-11-03 17:03:05 +0700

    Выполнил Этап 1 пункт 8

--- src/main/java/com/javaacademy/nuclearstation/NuclearStation.java
+++ src/main/java/com/javaacademy/nuclearstation/NuclearStation.java
@@ -3,17 +3,41 @@ package com.javaacademy.nuclearstation;
 import static java.math.BigDecimal.ZERO;
 
 import com.javaacademy.nuclearstation.reactordepartment.ReactorDepartment;
+import com.javaacademy.nuclearstation.reactordepartment.exception.NuclearFuelIsEmptyException;
+import com.javaacademy.nuclearstation.reactordepartment.exception.ReactorWorkException;
 import java.math.BigDecimal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class NuclearStation {
 
   private final ReactorDepartment reactorDepartment;
 
   private BigDecimal totalAmountOfEnergyGenerated = ZERO;
+  private BigDecimal amountOfEnergyGeneratedForYear = ZERO;
 
   public NuclearStation(ReactorDepartment reactorDepartment) {
     this.reactorDepartment = reactorDepartment;
+  }
+
+  public void startYear() throws ReactorWorkException {
+    log.info("Атомная станция начала работу");
+    for (int i = 0; i < 365; i++) {
+      try {
+        BigDecimal amountOfEnergyGeneratedPerDay = reactorDepartment.run();
+        amountOfEnergyGeneratedForYear = amountOfEnergyGeneratedForYear
+            .add(amountOfEnergyGeneratedPerDay);
+        reactorDepartment.stop();
+      } catch (NuclearFuelIsEmptyException e) {
+        log.info("Внимание! Происходят работы на атомной станции! Электричества нет!");
+      }
+    }
+
+    log.info("Атомная станция закончила работу. За год Выработано {} киловатт/часов",
+        amountOfEnergyGeneratedForYear);
+    totalAmountOfEnergyGenerated = totalAmountOfEnergyGenerated.add(amountOfEnergyGeneratedForYear);
+    amountOfEnergyGeneratedForYear = ZERO;
   }
 }
