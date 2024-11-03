9253962bce9bfebf1fad6e42485a30c2ea8aba3c
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-11-03 16:16:01 +0700

    Создал класс Runner и метод psvm, который будет поднимать контекст.

---
+++ src/main/java/com/javaacademy/atomicstation/Runner.java
@@ -0,0 +1,12 @@
+package com.javaacademy.atomicstation;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runner {
+
+  public static void main(String[] args) {
+
+  }
+
+}
